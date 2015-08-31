98f198f3fbd9b83e495c290b5a6f5466622e3f01
ddavisgraphics <ddavisgraphics@gmail.com>
2015-08-31 09:11:39 -0400

    template header.

--- cleanTemplate/templateHeader.php
+++ cleanTemplate/templateHeader.php
@@ -4,18 +4,31 @@
 
 	<title>{local var="currentDisplayObjectTitle"} | WVU Libraries | West Virginia University</title>
 
-	<!-- Meta Data -->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta Information -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="HandheldFriendly" content="True">
 
-        <meta name="keywords" content="{local var="currentDisplayObjectKeywords"}">
-        <meta name="description" content="{local var="currentDisplayObjectDescription"}">
-        <meta name="author" content="WVU Libraries">
-        <meta http-equiv="cleartype" content="on">
+    <!-- Author, Description, Favicon, and Keywords -->
+        <meta name="author" content="WVU Libraries | {local var="meta_authors"}">
+        <meta name="description" content="{local var="meta_description"}">
+        <meta name="keywords" content="{local var="meta_keywords"}">
+        <link rel="shortcut icon" href="https://wvrhc.lib.wvu.edu/favicon.ico">
 
 	 <!-- Project Specific Head Includes -->
 		<?php recurseInsert("headerIncludes.php","php") ?>
 </head>
 
 <body>
+
+ <!-- WebApp Header -->
+	<div class="webapp-header">
+	    <div class="wrap">
+	        <h1><a href="{local var="appURL"}">{local var="appTitle"}</a></h1>
+	    </div>
+	</div>
+
+ <!-- Navigation -->
+    <?php recurseInsert("includes/nav.php","php") ?>
+

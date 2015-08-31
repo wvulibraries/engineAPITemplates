a370e1c12034008575cc2572b9c30af69fb9aa5d
ddavisgraphics <ddavisgraphics@gmail.com>
2015-08-31 08:53:50 -0400

    Dependency Free Template
    Updated Meta Tags, Footer, etc.

---
+++ cleanTemplate/templateHeader.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>{local var="currentDisplayObjectTitle"} | WVU Libraries | West Virginia University</title>
+
+	<!-- Meta Data -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+        <meta name="keywords" content="{local var="currentDisplayObjectKeywords"}">
+        <meta name="description" content="{local var="currentDisplayObjectDescription"}">
+        <meta name="author" content="WVU Libraries">
+        <meta http-equiv="cleartype" content="on">
+
+	 <!-- Project Specific Head Includes -->
+		<?php recurseInsert("headerIncludes.php","php") ?>
+</head>
+
+<body>
